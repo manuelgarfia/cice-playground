4958f6c0007fe60447cfbff4428c20a5404fce71
Manolo Garfia <manuelgarfiaguerrero@hotmail.com>
2020-12-09 21:10:32 +0100

    esModules Ex1

---
+++ packages/6-webapp/esmodules/ex1/my-solutions/input-using-prompt.js
@@ -0,0 +1,6 @@
+const result = prompt('Introduce A,B or C', '?').toLowerCase()
+
+;(async function () {
+  const module = await import(`./${result}.js`)
+  console.warn(module.showLog())
+})()
---
+++ packages/6-webapp/esmodules/ex1/my-solutions/main.js
@@ -0,0 +1,13 @@
+import { getTime } from './get-second-time'
+
+export function getTime() {
+  console.log(getTime())
+}
+
+export function getName() {
+  alert('Manolo')
+}
+export function getConfirmedAlert() {
+  confirm('Congrats you are the 1M Th Visitor')
+  // window.alert("Congrats you are the 1M Th Visitor")
+}
